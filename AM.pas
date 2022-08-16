f44a4e2ff3b3dd8854ba6497dc4f735c90cf35a1
Sun Serega <sunserega2@gmail.com>
2022-08-17 01:53:29 +0300

    It works

---
+++ AM.pas
@@ -0,0 +1,255 @@
+﻿{$apptype windows}
+
+{$reference PresentationFramework.dll}
+{$reference PresentationCore.dll}
+{$reference WindowsBase.dll}
+
+uses System;
+uses System.Windows;
+uses System.Windows.Media;
+uses System.Windows.Controls;
+
+uses WinAPI;
+
+var enc := new System.Text.UTF8Encoding(true);
+procedure AppendLine(log, line: string);
+begin
+  System.IO.File.AppendAllLines(log, |line|, enc);
+  Console.Beep;
+//  Writeln((log,line));
+end;
+
+type
+  WinClassGroup = class;
+  WinClass = sealed class
+    name := default(string);
+    gr: WinClassGroup;
+    win_names := new HashSet<string>;
+    visual := new TreeViewItem;
+    arrow_l := new Button;
+    arrow_r := new Button;
+    
+    static All := new Dictionary<string, WinClass>;
+    
+    function GetLogName: string;
+    procedure RegisterArrowClicks;
+    
+    constructor(name: string; gr: WinClassGroup);
+    begin
+      self.name := name;
+      self.gr := gr;
+      
+      var head := new DockPanel;
+      
+      head.Children.Add(arrow_l);
+      DockPanel.SetDock(arrow_l, Dock.Left);
+      arrow_l.Content := '<';
+      arrow_l.Padding := new Thickness(0);
+      arrow_l.Margin := new Thickness(0,0,2,0);
+      
+      head.Children.Add(arrow_r);
+      DockPanel.SetDock(arrow_r, Dock.Left);
+      arrow_r.Content := '>';
+      arrow_r.Padding := new Thickness(0);
+      arrow_r.Margin := new Thickness(0,0,5,0);
+      
+      var head_tb := new TextBlock;
+      head.Children.Add(head_tb);
+      head_tb.Text := name;
+      
+      visual.Header := head;
+      
+      RegisterArrowClicks;
+      All.Add(name, self);
+    end;
+    
+    procedure AddWinName(win_name: string; save: boolean := true);
+    begin
+      if string.IsNullOrWhiteSpace(win_name) then exit;
+      if not win_names.Add(win_name) then exit;
+      if save then AppendLine(GetLogName, win_name);
+      visual.Items.Add(new TextBlock(new System.Windows.Documents.Run(win_name)));
+    end;
+    
+  end;
+  
+  WinClassGroup = sealed class
+    dir_name := default(string);
+    name := default(string);
+    classes := new HashSet<WinClass>;
+    visual := new TreeView;
+    
+    static All := new List<WinClassGroup>;
+    
+    constructor(name: string);
+    begin
+      self.dir_name := name;
+      var spl := name.Split(|' '|, 2);
+      if (spl.Length=2) and spl[0].All(char.IsDigit) then
+        name := spl[1];
+      self.name := name;
+      All.Add(self);
+    end;
+    
+    procedure InsertClassVisual(c: WinClass) :=
+    visual.Items.Insert(self.classes.Count(c0->c0.name<c.name), c.visual);
+    
+    function AddClass(win_class: string): WinClass;
+    begin
+      Result := new WinClass(win_class, self);
+      InsertClassVisual(Result);
+      if not classes.Add(Result) then raise new System.InvalidOperationException;
+    end;
+    
+    procedure RemoveClass(c: WinClass);
+    begin
+      if not classes.Remove(c) then
+        raise new System.InvalidOperationException;
+      visual.Items.Remove(c.visual);
+    end;
+    
+    procedure MoveClass(c: WinClass);
+    begin
+      c.gr.RemoveClass(c);
+      var log_fname := c.GetLogName;
+      InsertClassVisual(c);
+      if not classes.Add(c) then raise new System.InvalidOperationException;
+      c.gr := self;
+      System.IO.File.Move(log_fname, c.GetLogName);
+    end;
+    
+  end;
+  
+const
+  ClassesDir = 'Classes';
+  
+function WinClass.GetLogName := $'{ClassesDir}/{gr.dir_name}/{self.name}';
+
+procedure WinClass.RegisterArrowClicks;
+begin
+  var i := WinClassGroup.All.IndexOf(self.gr);
+  arrow_l.Click += (o,e)->
+  begin
+    if i=0 then exit;
+    i -= 1;
+    WinClassGroup.All[i].MoveClass(self);
+  end;
+  arrow_r.Click += (o,e)->
+  begin
+    if i=WinClassGroup.All.Count-1 then exit;
+    i += 1;
+    WinClassGroup.All[i].MoveClass(self);
+  end;
+end;
+
+const
+  GN_Touch  = 'Touch';
+  GN_What   = 'What';
+  GN_Dont   = 'Dont';
+
+function DefaultGroupName(cname: string): string;
+begin
+  
+  if cname.StartsWith('HwndWrapper[') then
+    Result := GN_Dont else
+  
+  Result := GN_What;
+end;
+
+begin
+  
+  foreach var gname in |GN_Touch, GN_What, GN_Dont| index i do
+  begin
+    var gr := new WinClassGroup(gname);
+    
+    var dir := $'{ClassesDir}\{i} {gname}';
+    System.IO.Directory.CreateDirectory(dir);
+    foreach var fname in EnumerateFiles(dir) do
+    begin
+      var c := gr.AddClass(System.IO.Path.GetFileName(fname));
+      foreach var win_name in ReadLines(fname, enc) do
+        c.AddWinName(win_name, false);
+    end;
+    
+  end;
+  
+  var un_max := new HashSet<WinAPIWindow>;
+  var TryMaximize := function(w: WinAPIWindow): boolean->
+  begin
+    Result := w.Maximize;
+    if Result then exit;
+    AppendLine('err.log', $'{DateTime.Now}: [{(w.GetClass()??string.Empty).PadRight(50)}] "{(w.GetName()??string.Empty).PadRight(100)}" | Failed to maximize');
+  end;
+  
+  var w := new Window;
+  w.WindowState := WindowState.Maximized;
+  
+  var g := new Grid;
+  w.Content := g;
+  
+  loop 2 do g.RowDefinitions.Add(new RowDefinition);
+  g.RowDefinitions[0].Height := GridLength.Auto;
+  
+  foreach var gr in WinClassGroup.All index i do
+  begin
+    
+    var cd := new ColumnDefinition;
+    cd.Width := new GridLength(1, GridUnitType.Star);
+    g.ColumnDefinitions.Add(cd);
+    
+    var title := new TextBlock;
+    g.Children.Add(title);
+    title.Text := gr.name;
+    title.HorizontalAlignment := HorizontalAlignment.Center;
+    Grid.SetColumn(title, i);
+    
+    g.Children.Add(gr.visual);
+    Grid.SetRow(gr.visual, 1);
+    Grid.SetColumn(gr.visual, i);
+    
+  end;
+  
+  System.Threading.Thread.Create(()->
+  while true do
+  begin
+    un_max.RemoveWhere(TryMaximize);
+    
+    LongPollWindows(w->
+    begin
+//      $'{DateTime.Now}: [{(w.GetClass()??string.Empty).PadRight(50)}] "{(w.GetName()??string.Empty).PadRight(100)}"'.Println;
+      
+      if w.GetPlacement.showCmd<>WinAPIWindowShowState.SW_NORMAL then exit;
+      
+      var cname := w.GetClass;
+      if cname=nil then exit;
+      
+      g.Dispatcher.Invoke(()->
+      begin
+        
+        var c: WinClass;
+        if not WinClass.All.TryGetValue(cname, c) then
+        begin
+          var gname := DefaultGroupName(cname);
+          var gr := WinClassGroup.All.Single(gr->gr.name=gname);
+          c := gr.AddClass(cname);
+          System.IO.File.Create(c.GetLogName).Close;
+          
+        end;
+        
+        if c.gr.name=GN_Touch then
+        begin
+          w.Maximize;
+          un_max += w;
+        end;
+        
+        c.AddWinName(w.GetName);
+      end);
+      
+    end);
+    
+    Sleep(100);
+  end).Start;
+  
+  var ec := Application.Create.Run(w);
+  Halt(ec);
+end.
---
+++ WinAPI.pas
@@ -0,0 +1,170 @@
+﻿unit WinAPI;
+
+interface
+
+uses System;
+
+type
+  WinAPIWindowShowState = (
+    SW_HIDE = 0,            // Hides the window and activates another window.
+    SW_NORMAL = 1,          // Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time.
+    SW_SHOWMINIMIZED = 2,   // Activates the window and displays it as a minimized window.
+    SW_MAXIMIZE = 3,        // Activates the window and displays it as a maximized window.
+    SW_SHOWNOACTIVATE = 4,  // Displays a window in its most recent size and position. This value is similar to SW_SHOWNORMAL, except that the window is not activated.
+    SW_SHOW = 5,            // Activates the window and displays it in its current size and position.
+    SW_MINIMIZE = 6,        // Minimizes the specified window and activates the next top-level window in the Z order.
+    SW_SHOWMINNOACTIVE = 7, // Displays the window as a minimized window. This value is similar to SW_SHOWMINIMIZED, except the window is not activated.
+    SW_SHOWNA = 8,          // Displays the window in its current size and position. This value is similar to SW_SHOW, except that the window is not activated.
+    SW_RESTORE = 9,         // Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when restoring a minimized window.
+    SW_SHOWDEFAULT = 10,    // Sets the show state based on the SW_ value specified in the STARTUPINFO structure passed to the CreateProcess function by the program that started the application.
+    SW_FORCEMINIMIZE = 11   // Minimizes a window, even if the thread that owns the window is not responding. This flag should only be used when minimizing windows from a different thread.
+  );
+  
+  WinAPIWindowPlacement = record
+    length: UInt32 := sizeof(WinAPIWindowPlacement);
+    flags: UInt32;
+    showCmd: WinAPIWindowShowState;
+    ptMinPosition, ptMaxPosition: record
+      X, Y: Int32;
+    end;
+    rcNormalPosition, rcDevice: record
+      X1,Y1: Int32;
+      X2,Y2: Int32;
+    end;
+  end;
+  
+  WinAPIWindow = record
+    private hnd: IntPtr;
+    
+    public function GetClass: string;
+    
+    public function GetName: string;
+    
+    public function GetParent: WinAPIWindow;
+    
+    public function GetPlacement: WinAPIWindowPlacement;
+    
+    public function Maximize: boolean;
+    
+  end;
+  
+procedure LongPollWindows(new_win: Action<WinAPIWindow>);
+
+implementation
+
+uses System.Runtime.InteropServices;
+
+{$region Win info}
+
+type
+  StringGetHelper = static class
+    
+    private [ThreadStatic] static curr_buff: record
+      ptr: IntPtr;
+      max_len: integer;
+    end;
+    
+    private static function GetCharBuff(len: integer): IntPtr;
+    begin
+      var res := curr_buff;
+      
+      Result := res.ptr;
+      if (Result<>IntPtr.Zero) and (res.max_len>=len) then exit;
+      
+      Result := Marshal.AllocHGlobal(len);
+      Marshal.FreeHGlobal(res.ptr);
+      res.ptr := Result;
+      res.max_len := len;
+      
+      curr_buff := res;
+    end;
+    
+    public static function GetString(win: WinAPIWindow; len: integer; f: function(win: WinAPIWindow; buff: IntPtr; max_len: integer): integer): string;
+    begin
+      var buff := GetCharBuff(len);
+      Result := if f(win, buff, len)=0 then
+        nil else Marshal.PtrToStringAnsi(buff);
+    end;
+    
+  end;
+
+function GetClassName(hnd: WinAPIWindow; buff: IntPtr; max_len: integer): integer;
+external 'user32.dll';
+function WinAPIWindow.GetClass :=
+StringGetHelper.GetString(self, 256, GetClassName);
+
+function GetWindowTextLength(hnd: WinAPIWindow): integer;
+external 'user32.dll';
+function GetWindowText(hnd: WinAPIWindow; buff: IntPtr; max_len: integer): integer;
+external 'user32.dll';
+
+function WinAPIWindow.GetName :=
+StringGetHelper.GetString(self, GetWindowTextLength(self)+1, GetWindowText);
+
+function GetParent(win: WinAPIWindow): WinAPIWindow;
+external 'user32.dll';
+function WinAPIWindow.GetParent: WinAPIWindow;
+begin
+  
+end;
+
+{$endregion Win info}
+
+{$region LongPollWindows}
+
+function EnumWindows(on_win: function(hnd: WinAPIWindow; data: IntPtr): boolean; data: IntPtr): boolean;
+external 'user32.dll';
+function IsWindowVisible(win: WinAPIWindow): boolean;
+external 'user32.dll';
+
+var last_windows := new HashSet<WinAPIWindow>;
+procedure LongPollWindows(new_win: Action<WinAPIWindow>);
+begin
+  var curr_windows := new HashSet<WinAPIWindow>(last_windows.Count);
+  
+  if not EnumWindows((win,data)->
+  begin
+    Result := true;
+    if not IsWindowVisible(win) then exit;
+    if win not in last_windows then
+      new_win(win);
+    if not curr_windows.Add(win) then
+      raise new System.InvalidOperationException;
+  end, IntPtr.Zero) then
+    raise new System.ComponentModel.Win32Exception;
+  
+  last_windows := curr_windows;
+end;
+
+{$endregion LongPollWindows}
+
+{$region Maximize}
+
+function PostMessage(win: WinAPIWindow; msg: UInt32; w_param: UIntPtr; l_param: IntPtr): boolean;
+external 'user32.dll';
+
+function GetWindowPlacement(win: WinAPIWindow; var pl: WinAPIWindowPlacement): boolean;
+external 'user32.dll';
+
+function WinAPIWindow.GetPlacement: WinAPIWindowPlacement;
+begin
+  if not GetWindowPlacement(self, Result) then
+    raise new System.ComponentModel.Win32Exception;
+end;
+
+function WinAPIWindow.Maximize: boolean;
+const WM_SYSCOMMAND = $0112;
+const SC_MAXIMIZE   = $F030;
+begin
+  Result := false;
+  
+  if not PostMessage(self, WM_SYSCOMMAND, new UIntPtr(SC_MAXIMIZE), IntPtr.Zero) then
+    exit;
+//    new System.ComponentModel.Win32Exception;
+  
+  Result := GetPlacement.showCmd=WinAPIWindowShowState.SW_MAXIMIZE;
+end;
+
+{$endregion Maximize}
+
+end.
